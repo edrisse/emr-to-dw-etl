5ce1ee9fce6f854d3f3864007b0a16f48bbc37ee
Edrisse Mussá <edrisseasm@gmail.com>
2018-01-30 17:30:18 +0200

    We now mark the ART_STATUS records that represent begining of treatment

--- etc/esaude_dw.sql
+++ etc/esaude_dw.sql
@@ -1,5 +1,4 @@
 drop table pregnancy;
-drop table breastfeed;
 drop table art_status;
 drop table patient;
 drop table location;
@@ -40,8 +39,7 @@ create table art_status (
   notified bit,
   age_in_months int(2),
   age_in_years int(2),
-  pregnant bit,
-  breastfeeding bit,
+  first_event bit default false,
   primary key (id)
 );
 alter table art_status add constraint fk_tarv_status_patient foreign key (patient_id) references patient (id);
